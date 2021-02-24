5df94d150a0452b127b837b336ebe2cfc2597599
Giovanni <giovannitadeu08@gmail.com>
2021-02-24 19:21:01 -0300

    Conteudos do bloco 4 dia 1

---
+++ trybe-exercises/exercises/bloco_4/JS-PRIMEIROS-PASSOS/scripts.js
@@ -0,0 +1,15 @@
+let estado = 'ap';
+
+switch (estado) {
+    case 'aprovada':
+        console.log('Aprovado')
+        break;
+    case 'lista':
+        console.log('Na lista')
+        break;
+    case 'reprovado':
+        console.log('Reprovado')
+        break;
+    default:
+        console.log('Não se aplica')
+}
