76e35b47937efb5eabaf0ff7b6cc87dac6d927b3
Giovanni <giovannitadeu08@gmail.com>
2021-02-25 20:18:41 -0300

    [4] Exercicio - Pronto

--- trybe-exercises/exercises/bloco_4/Array-e-for-loop/script.js
+++ trybe-exercises/exercises/bloco_4/Array-e-for-loop/script.js
@@ -25,3 +25,18 @@ for (index of numbers) {
 media = soma/numbers.length;
 console.log(media);
 
+//Exercicio 4
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let media;
+for (index of numbers) {
+  soma += index;
+}
+media = soma/numbers.length
+console.log(media)
+if (media > 20){
+    console.log('A media é maior que 20');
+}else{
+    console.log('A media é menor que 20');
+}
