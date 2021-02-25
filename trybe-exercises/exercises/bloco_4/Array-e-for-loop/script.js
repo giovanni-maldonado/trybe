973f099023a66fd6be9620d8a95f5f2e5a60c596
Giovanni <giovannitadeu08@gmail.com>
2021-02-25 20:15:44 -0300

    [3] Exercicio - Pronto

--- trybe-exercises/exercises/bloco_4/Array-e-for-loop/script.js
+++ trybe-exercises/exercises/bloco_4/Array-e-for-loop/script.js
@@ -12,4 +12,16 @@ let soma = 0;
 for(let index = 0; index < numbers.length; index++ ){
     soma =+ numbers[index]
     console.log(soma);
+}   
+
+//Exercicio 3
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let media;
+for (index of numbers) {
+  soma += index;
 }
+media = soma/numbers.length;
+console.log(media);
+
