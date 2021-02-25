4ee305940b5ce6cbaa35f7006acdb718fcbe5dd4
Giovanni <giovannitadeu08@gmail.com>
2021-02-25 20:21:37 -0300

    [7] Exercicio - Pronto

--- trybe-exercises/exercises/bloco_4/Array-e-for-loop/script.js
+++ trybe-exercises/exercises/bloco_4/Array-e-for-loop/script.js
@@ -67,3 +67,14 @@ if(contImpares > 0){
 }else{
     console.log('Não tem numeros impares')
 }
+
+//Exercicio 7 
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menorValor = numbers[0];
+for(let index of numbers){
+    if (menorValor > index) {
+      menorValor = index;
+    }
+}
+console.log(menorValor);
