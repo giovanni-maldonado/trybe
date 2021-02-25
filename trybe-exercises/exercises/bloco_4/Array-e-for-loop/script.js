6a695f581ce3261ca956c6d581dc0b4015dc7a26
Giovanni <giovannitadeu08@gmail.com>
2021-02-25 20:14:41 -0300

    [2] Exercicio - Pronto

--- trybe-exercises/exercises/bloco_4/Array-e-for-loop/script.js
+++ trybe-exercises/exercises/bloco_4/Array-e-for-loop/script.js
@@ -1,3 +1,10 @@
+//Exercicio 1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for(let index = 0; index < numbers.length; index++){
+  console.log(numbers[index]);
+}
+
 //Exercicio 2
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
@@ -6,72 +13,3 @@ for(let index = 0; index < numbers.length; index++ ){
     soma =+ numbers[index]
     console.log(soma);
 }
-
-//Exercicio 3
-
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
-let media;
-for (index of numbers) {
-  soma += index;
-}
-media = soma/numbers.length;
-console.log(media);
-
-//Exercicio 4
-
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
-let media;
-for (index of numbers) {
-  soma += index;
-}
-media = soma/numbers.length
-console.log(media)
-if (media > 20){
-    console.log('A media é maior que 20');
-}else{
-    console.log('A media é menor que 20');
-}
-
-//Exercicio 5
-
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maiorNumero = 0;
-for (let index of numbers) {
-  if (maiorNumero < index) {
-    maiorNumero = index;
-  }
-}
-console.log(maiorNumero);
-
-//Exercicio 6 
-
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let contImpares = 0;
-
-for (let index of numbers){
-    if (index % 2 == 1){
-        contImpares++
-    }
-}
-if(contImpares > 0){
-    console.log('Tem ',contImpares, ' numeros impares' )
-}else{
-    console.log('Não tem numeros impares')
-}
-
-//Exercicio 7 
-
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let menorValor = numbers[0];
-for(let index of numbers){
-    if (menorValor > index) {
-      menorValor = index;
-    }
-}
-console.log(menorValor);
-
-//Exercicio 8
-
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
