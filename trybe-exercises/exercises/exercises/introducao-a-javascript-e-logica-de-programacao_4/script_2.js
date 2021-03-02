06b2a2f4476e71b66e2b9f1980762ba9d8f5eaa4
Giovanni <giovannitadeu08@gmail.com>
2021-03-01 21:26:22 -0300

    Exercise 4 - Done

--- trybe-exercises/exercises/exercises/introducao-a-javascript-e-logica-de-programacao_4/script_2.js
+++ trybe-exercises/exercises/exercises/introducao-a-javascript-e-logica-de-programacao_4/script_2.js
@@ -46,3 +46,17 @@ function indiceMenorValor() {
 
 let inteiros = [2, 4, 6, 7, 10, 0, -3];
 console.log(indiceMenorValor(inteiros));
+
+//4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+function maiorPalavra(palavras) {
+    let maiorPalavra = palavras[0];
+    for (let indice in palavras) {
+      if (maiorPalavra.length < palavras[indice].length) {
+        maiorPalavra = palavras[indice];
+      }
+    }
+    return maiorPalavra;
+  }
+  
+  console.log(maiorPalavra(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
